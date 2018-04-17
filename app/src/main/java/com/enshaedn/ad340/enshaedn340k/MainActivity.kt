62a5e741183b34d0b5c0fcc163a273feee0e2e04
Alex Clontz <alexclontz@gmail.com>
2018-04-17 15:56:28 -0700

    added grid buttons and toasts

--- app/src/main/java/com/enshaedn/ad340/enshaedn340k/MainActivity.kt
+++ app/src/main/java/com/enshaedn/ad340/enshaedn340k/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 const val PASS_PHRASE = "com.enshaedn.ad340.enshaedn340k.MESSAGE"
 
@@ -23,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             putExtra(PASS_PHRASE, phrase)
         }
         startActivity(intent)
+    }
+    //toast duration
+    private val tDur = Toast.LENGTH_SHORT
+    /*
+    fun gridClicked(view: View, button: Button) {
+        when(button.id) {
+            gridb2.id -> Toast.makeText(applicationContext, "Button 2 clicked!", tDur).show()
+            gridb3.id -> Toast.makeText(applicationContext, "Button 3 clicked!", tDur).show()
+            gridb4.id -> Toast.makeText(applicationContext, "Button 4 clicked!", tDur).show()
+        }
+    }*/
+
+    //toast message for button 2
+    fun gridB2Clicked(view: View) {
+        Toast.makeText(applicationContext, "Button 2 clicked!", tDur).show()
+    }
+
+    //toast message for button 3
+    fun gridB3Clicked(view: View) {
+        Toast.makeText(applicationContext, "Button 3 clicked!", tDur).show()
+    }
+
+    //toast message for button 4
+    fun gridB4Clicked(view: View) {
+        Toast.makeText(applicationContext, "Button 4 clicked!", tDur).show()
     }
 }
